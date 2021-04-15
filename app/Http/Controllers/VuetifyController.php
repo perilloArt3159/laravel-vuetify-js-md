2e539945866aa1c739269d2f445ead239d31aeab
LAZYASSKID <lzysskd@gmail.com>
2021-04-16 03:49:52 +0800

    Initial Commit

---
+++ app/Http/Controllers/VuetifyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class VuetifyController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+}
---
+++ vuetify/vue.config.js
@@ -0,0 +1,15 @@
+const { env } = require("shelljs");
+
+module.exports = {
+  devServer: {
+    // set proxy address
+    proxy: 'http://127.0.0.1:8000',
+    disableHostCheck: true
+  },
+  // save static files in laravel public dir
+  outputDir: '../public',
+  // generated html files location during production
+  indexPath: process.env.NODE_ENV === 'production' ? '../resources/views/index.blade.php' : 'index.html',
+  // vuetify
+  transpileDependencies: ['vuetify'],
+}
